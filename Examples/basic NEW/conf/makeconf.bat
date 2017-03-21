a1f61b532edd2b340f908df9bb59cec77af944f1
diubaldo94 <diubaldo94@gmail.com>
2017-03-21 13:56:30 +0100

    Delete makeconf.bat

--- Examples/basic NEW/conf/makeconf.bat
+++
@@ -1,4 +0,0 @@
-if not exist conf\communication_%1.con (
-	copy ..\..\src\communication_default\communication.con conf\communication_%1.con	
-)
-echo agent('work/%1',%1,'no',italian,['conf/communication_%1'],['../../src/communication_fipa','../../src/learning','../../src/planasp'],'no','../../src/onto/dali_onto.txt',[]). > conf/%2 
